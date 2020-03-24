79f94e8e96ad20e86a03a3cd8ba47955d75c7fa0
jastaffen <jack.staffen@gmail.com>
2020-03-24 00:47:56 -0400

    unsave video route

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
-// app.use('/api/comments', require('./routes/api/comments'));
+app.use('/api/comments', require('./routes/api/comments'));
 app.use('/api/videos', require('./routes/api/videos'));
 
 
