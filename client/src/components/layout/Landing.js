399d0bc6e802b4be0ed3fc109190ab8831c287a6
jastaffen <jack.staffen@gmail.com>
2020-03-24 19:27:45 -0400

    clean up frontend

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <section>
+            <div>
+                <h1>LiveShows+</h1>
+                <p>Watch full concerts by your favorite artists</p>
+                <div>
+                    <a href="index.html">Sign Up</a>
+                    <a href="index.html">Login</a>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        
+    <nav>
+            <h1>
+                <a href="index.html">LiveShows+</a>
+            </h1>
+        <ul>
+            <li>Sign Up</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar;
